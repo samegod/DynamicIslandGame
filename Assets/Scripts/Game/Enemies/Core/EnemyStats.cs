8eb403685fa2ad8a4ecf1126c85c0f29f0a1ec8c
a.zhuk <azhuk@psvgamestudio.com>
2024-03-27 22:18:20 +0200

    - Stats
    > дороблено стати та часткову ініціалізацію

--- Assets/Scripts/Game/Enemies/Core/EnemyStats.cs
+++ Assets/Scripts/Game/Enemies/Core/EnemyStats.cs
@@ -7,8 +7,14 @@ namespace Enemies.Core
     {
         [SerializeField] private float health;
         [SerializeField] private float speed;
+        [SerializeField] private float damage;
+        [SerializeField] private float attackRange;
+        [SerializeField] private float fireRate;
 
         public float Health => health;
         public float Speed => speed;
+        public float Damage => damage;
+        public float AttackRange => attackRange;
+        public float FireRate => fireRate;
     }
 }
