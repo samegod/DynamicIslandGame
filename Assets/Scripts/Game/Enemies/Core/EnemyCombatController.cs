db662f2c04652e2903d9e41ce6ff6adaabba9285
Ninel Levchenko <blackmaplearts@gmail.com>
2024-04-02 12:11:47 +0300

    - EnemyCombatController
    >додано enum типу атаки

--- Assets/Scripts/Game/Enemies/Core/EnemyCombatController.cs
+++ Assets/Scripts/Game/Enemies/Core/EnemyCombatController.cs
@@ -3,9 +3,18 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+public enum CombatType
+{ 
+    Easy,
+    Medium,
+    Hard
+}
 [System.Serializable]
 public struct CombatSettings {
     public float damage;
+    public CombatType type;
+    public int variationCount;
 }
 public class EnemyCombatController : MonoBehaviour
 {
